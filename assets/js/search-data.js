0f5448c5dd4b9ba9d669e75ec9275560b1b33d6e
John-Ge <41175708+John-Ge@users.noreply.github.com>
2025-02-23 06:32:33 +0000

    Deploying to gh-pages from @ John-Ge/Awesome-Reasoning-Research@0524e68f63bdcfdea2e75c6775bd28126e9df3a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "dropdown-projects",
-              title: "projects",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
             },{id: "dropdown-blog",
               title: "blog",
               description: "",
